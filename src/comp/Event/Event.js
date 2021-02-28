39f65d4d94e2991d962aea802113404fba2acf09
Victorowsky <Victorowskyy@gmail.com>
2021-02-28 13:13:04 +0100

    profile button in event

--- src/comp/Event/Event.js
+++ src/comp/Event/Event.js
@@ -4,6 +4,7 @@ import { DataContext } from "../../App";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import "./Event.css";
 import { IconButton } from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 const Event = ({ currentEvent }) => {
 	const { userInfo } = useContext(DataContext);
 	const history = useHistory();
@@ -18,15 +19,27 @@ const Event = ({ currentEvent }) => {
 		window.location.href = "/logout";
 	};
 
+	const handleProfile = () =>{
+		history.push('/profile')
+	}
+
+	const iconButtonStyles = {
+		 color: "white", height: "fit-content" 
+	}
+
 	return (
 		<div className="event">
-			<IconButton style={{color:'white', height:'fit-content'}}>
-				<ExitToAppIcon onClick={handleLogout} className="logoutButton" />
+			<IconButton style={iconButtonStyles} onClick={handleLogout}>
+				<ExitToAppIcon  className="logoutButton" />
 			</IconButton>
 			<div className="eventName" onClick={handleRedirectToAdmin}>
 				<h2>{currentEvent?.name}</h2>
 			</div>
-			<div style={{ width: '48px' }} className="logoutButton"></div>
+			<div style={{ width: "48px" }} className="logoutButton">
+				<IconButton style={iconButtonStyles} onClick={handleProfile}>
+					<AccountCircleIcon  />
+				</IconButton>
+			</div>
 		</div>
 	);
 };
